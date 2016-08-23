909d5e9b34bb4513b53844c8357d81019b7060f4
Lt-grint <1598552894@qq.com>
2016-08-23 18:43:58 +0800

    add 387 solution

--- java/283 Move Zeroes/Solution.java
+++ java/283 Move Zeroes/Solution.java
@@ -1,12 +1,14 @@
 ﻿/************author: litao******************/
 /************date:2016/08/23****************/
-
+/************number:283*********************/
+/************title:move zeros**************/
 /*********************************************
-*
+*  Describe:
 *  Given an array nums, write a function to move 
 *  all 0's to the end of it while maintaining the 
 *  relative order of the non-zero elements.
 *
+*  Examples:
 *  For example, given nums = [0, 1, 0, 3, 12], after
 *  calling your function, nums should be [1, 3, 12, 0, 0].
 *
---
+++ java/387 First Unique Character/Solution.java
@@ -0,0 +1,42 @@
+﻿/************author: litao******************/
+/************date:2016/08/23****************/
+/************number:387*********************/
+/************title:First Unique Character in a String*/
+/*********************************************
+*  Describe:
+*  Given a string, find the first non-repeating 
+*  character in it and return it's index. If it 
+*   doesn't exist, return -1.
+*
+*  Examples:
+*  s = "leetcode"
+*  return 0.
+*  s = "loveleetcode",
+*  return 2.
+*
+**********************************************/
+public class Solution {
+    public int firstUniqChar(String s) {
+        int[] arr=new int[26];
+        int len=s.length();
+        for(int i=0;i<len;i++){
+            char x=s.charAt(i);
+            //题目假设只有小写，若出现大写则返回-1
+            if(Character.isUpperCase(x)){
+                return -1;
+            }
+            arr[x-'a']++;
+        }
+        for(int i=0;i<len;i++){
+            char x=s.charAt(i);
+            //题目假设只有小写，若出现大写则返回-1
+            if(Character.isUpperCase(x)){
+                return -1;
+            }
+            if(arr[x-'a'] == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
