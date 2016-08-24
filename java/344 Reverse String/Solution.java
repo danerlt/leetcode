0d4eec8ab300d221f372a8479a14c48683e8b88c
Lt-grint <1598552894@qq.com>
2016-08-24 09:47:09 +0800

    add 344 reverse string solution

---
+++ java/344 Reverse String/Solution.java
@@ -0,0 +1,29 @@
+﻿/************author: litao******************/
+/************date:2016/08/24****************/
+/************number:344*********************/
+/************title:reverse string**************/
+/*********************************************
+*  Describe:
+*  Write a function that takes a string as input
+*  and returns the string reversed.
+*
+*  Examples:
+*  Given s = "hello", return "olleh".
+*
+**********************************************/
+public class Solution{
+	public String reverseString(String s){
+		int i=0,j=s.length()-1;
+		char[] str=s.toCharArray();
+		while(i<j){
+			char temp=str[i];
+			str[i]=str[j];
+			str[j]=temp;
+			i++;
+			j--;
+		
+		}
+		return String.copyValueOf(str);
+	}
+
+}
