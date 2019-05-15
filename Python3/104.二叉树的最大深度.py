cbc11c2f8847f601e6db74d41dd95f2fb6be5704
danerlt <danerlt001@gmail.com>
2019-05-15 22:59:37 +0800

    add 104 py3 solution

--- Python3/104.二叉树的最大深度.py
+++ Python3/104.二叉树的最大深度.py
@@ -12,5 +12,8 @@
 
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
-        
+        if not root: return 0
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        return max(left, right) + 1
 
