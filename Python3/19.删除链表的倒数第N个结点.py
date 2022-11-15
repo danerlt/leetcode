e3e033b790c58cbe15f009954c30689a06434cb9
danerlt <danerlt001@gmail.com>
2022-11-15 11:21:05 +0800

    feat python add 19 solution

---
+++ Python3/19.删除链表的倒数第N个结点.py
@@ -0,0 +1,76 @@
+# 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。 
+# 
+#  
+# 
+#  示例 1： 
+#  
+#  
+# 输入：head = [1,2,3,4,5], n = 2
+# 输出：[1,2,3,5]
+#  
+# 
+#  示例 2： 
+# 
+#  
+# 输入：head = [1], n = 1
+# 输出：[]
+#  
+# 
+#  示例 3： 
+# 
+#  
+# 输入：head = [1,2], n = 1
+# 输出：[1]
+#  
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  链表中结点的数目为 sz 
+#  1 <= sz <= 30 
+#  0 <= Node.val <= 100 
+#  1 <= n <= sz 
+#  
+# 
+#  
+# 
+#  进阶：你能尝试使用一趟扫描实现吗？ 
+# 
+#  Related Topics 链表 双指针 👍 2306 👎 0
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+        # 注意边界情况
+        dummy = ListNode(-1)
+        dummy.next = head
+
+        p1 = head
+        for i in range(n):
+            p1 = p1.next
+
+        p2 = dummy
+        while p1:
+            p1 = p1.next
+            p2 = p2.next
+
+        p2.next = p2.next.next
+
+        return dummy.next
+
+# leetcode submit region end(Prohibit modification and deletion)
